77179004d0c65e0cfcf941ac40ebfce356ba461a
Shreejan Maharjan <105190748+Ashura2003@users.noreply.github.com>
2023-06-21 17:33:05 +0545

    Added a text field and a button for User feedback

--- lib/screens/CustomerServiceScreen.dart
+++ lib/screens/CustomerServiceScreen.dart
@@ -9,12 +9,36 @@ class CustomerServiceScreen extends StatefulWidget {
 }
 
 class _CustomerServiceScreenState extends State<CustomerServiceScreen> {
+  String userFeedback = '';
+
+  void submitFeedback() {
+    // Implement your logic to handle the user feedback submission
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Feedback Submitted'),
+          content: Text('Thank you for your feedback!'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(
-      title: Text('Customer Service'),
-      backgroundColor: Colors.teal,
-    ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Customer Service'),
+        backgroundColor: Colors.teal,
+      ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
@@ -60,10 +84,36 @@ class _CustomerServiceScreenState extends State<CustomerServiceScreen> {
                 Icon(Icons.location_on, size: 20.0),
                 SizedBox(width: 8.0),
                 Text(
-                  'Address: Dillibazar,Kathmandu, Nepal',
+                  'Address: Dillibazar, Kathmandu, Nepal',
                   style: TextStyle(fontSize: 16.0),
                 ),
               ],
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'User Feedback:',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            TextFormField(
+              onChanged: (value) {
+                setState(() {
+                  userFeedback = value;
+                });
+              },
+              maxLines: 4,
+              decoration: InputDecoration(
+                hintText: 'Enter your feedback...',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: submitFeedback,
+              child: Text('Submit Feedback'),
             ),
           ],
         ),
