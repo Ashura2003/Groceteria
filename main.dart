f542c6d226d121c888e7d8ec5ff2cfcf71ee7a0f
emilkaagni <105575190+emilkaagni@users.noreply.github.com>
2023-06-28 09:08:49 +0545

    Paymentmain
    main.dart as a follow up for payment system

---
+++ main.dart
@@ -0,0 +1,56 @@
+import 'package:Groceteria/screens/cart.dart';
+import 'package:Groceteria/screens/dashboard.dart';
+import 'package:Groceteria/screens/payment.dart';
+import 'package:Groceteria/screens/registration.dart';
+import 'package:Groceteria/screens/vegetable_detail.dart';
+import 'package:Groceteria/screens/vegetables.dart';
+import 'package:Groceteria/screens/welcome.dart';
+import 'package:Groceteria/services/NotificationService.dart';
+import 'package:Groceteria/utils/routes.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:khalti_flutter/khalti_flutter.dart';
+
+
+
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  NotificationService.initialize();
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return KhaltiScope(
+      publicKey: 'test_public_key_efebe934195b44388c84c599d2b2b4eb', // Replace with your Khalti public key
+      enabledDebugging: true, // Set to false to disable network logs
+      builder: (context, navKey) {
+        return MaterialApp(
+          title: "Grocery App",
+          theme: ThemeData(fontFamily: GoogleFonts.lato().fontFamily),
+          navigatorKey: navKey,
+          localizationsDelegates: const [
+            KhaltiLocalizations.delegate,
+          ], // Provide the navigatorKey from KhaltiScope
+          initialRoute: MyRoutes.paymentRoute,
+          routes: {
+            // Define your routes here
+            MyRoutes.paymentRoute: ((context) => Payment()),
+            MyRoutes.welcomeRoute: (context) => WelcomeScreen(),
+            MyRoutes.registrationRoute: (context) => RegistrationScreen(),
+            MyRoutes.dashboardRoute: (context) => DashboardScreen(),
+            MyRoutes.vegetablesRoute: (context) => VegetablesScreen(),
+            MyRoutes.vegetableDetailRoute: (context) => VegetableDetailScreen(),
+            MyRoutes.cartRoute: (context) => CartScreen(),
+            // MyRoutes.notificationDemoRoute: (context) => NotificationDemo(),
+          },
+        );
+      },
+    );
+  }
+}
